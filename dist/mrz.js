c12073d5e702760417120e091b77d3fe6d7a2dbf
Mark Vayngrib <mark@tradle.io>
2017-01-03 09:17:34 +0800

    dist

--- dist/mrz.js
+++ dist/mrz.js
@@ -82,14 +82,19 @@ return /******/ (function(modules) { // webpackBootstrap
 	'use strict';
 
 	var parseTD1 = __webpack_require__(3);
-	var parseTD3 = __webpack_require__(5);
+	var parseTD2 = __webpack_require__(5);
+	var parseTD3 = __webpack_require__(6);
 
 	module.exports = function parse(text) {
 	    var lines = text.split(/[\r\n]+/);
 	    var result = {logs: []};
 	    switch (lines.length) {
 	        case 2:
-	            result = parseTD3(lines);
+	            if (lines[0].length < 41) {
+	                result = parseTD2(lines);
+	            } else {
+	                result = parseTD3(lines);
+	            }
 	            break;
 	        case 3:
 	            result = parseTD1(lines);
@@ -141,8 +146,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	    result.format = 'TD3';
 	    result.documentType = parseDocumentType(first.substring(0, 2));
 	    result.issuingCountry = parseCountry(parseText(first, 2, 5));
-	    result.documentNumber = parseDocumentNumber(parseText(first, 5, 14), first.substr(14, 1));
 	    result.optional1 = parseText(first, 15, 30);
+	    result.documentNumber = parseDocumentNumber(parseText(first, 5, 14), first.substr(14, 1), result.optional1);
+
 
 	    result.birthDate = parseDate(parseText(second, 0, 6), second.substr(6, 1));
 	    result.sex = parseSex(second.substr(7, 1));
@@ -169,7 +175,18 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	const COUNTRIES = __webpack_require__(1);
 
-	function parseDocumentNumber(value, checkDigit) {
+
+	/*
+	Parsing document number
+	The number may be splited (TD1 format)
+	 */
+
+	function parseDocumentNumber(value, checkDigit, optional) {
+	    if (checkDigit === '<' && optional) {
+	        optional = optional.replace(/<.*/, '');
+	        value += optional.substring(0, optional.length - 1);
+	        checkDigit = optional.charAt(optional.length - 1);
+	    }
 	    var result = {
 	        value,
 	        isValid: check(value, checkDigit)
@@ -177,7 +194,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    if (!result.isValid) {
 	        result.error = 'Check digit "' + checkDigit + '" not valid';
 	    }
-
+	    return result;
 	}
 
 	function parseDocumentType(code) {
@@ -308,6 +325,58 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = function parseTD3(lines) {
 	    var result = {isValid: true};
 	    var logs = [];
+	    logs.push('Parsing TD2 format');
+	    var first = lines[0];
+	    if (first.length !== 36) {
+	        result.isValid = false;
+	        logs.push('First line does not have 36 symbols');
+	    }
+	    var second = lines[1];
+	    if (second.length !== 36) {
+	        result.isValid = false;
+	        logs.push('Second line does not have 36 symbols');
+	    }
+	    result.format = 'TD2';
+	    result.documentType = parseDocumentType(first.substring(0, 2));
+	    result.issuingCountry = parseCountry(parseText(first, 2, 5));
+	    result.lastname = parseText(first, 5, 36).replace(/ {2}.*/, '');
+	    result.firstname = parseText(first, 5, 36).replace(/.* {2}/, '');
+	    result.documentNumber = parseDocumentNumber(parseText(second, 0, 9), second.substr(9, 1), second.substr(28, 35));
+	    result.nationality = parseCountry(parseText(second, 10, 13));
+	    result.birthDate = parseDate(parseText(second, 13, 19), second.substr(19, 1));
+
+	    result.sex = parseSex(parseText(second, 20, 21));
+
+	    result.expirationDate = parseDate(parseText(second, 21, 27), second.substr(27, 1));
+
+	    if (result.isValid) result.isValid = check(second.substring(0, 10) + second.substring(13, 20) + second.substring(21, 35), second.substr(35, 1));
+
+	    logs.push('TD2 parse completed');
+
+	    result.logs = logs;
+	    return result;
+	};
+
+
+/***/ },
+/* 6 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var {
+	    check,
+	    parseText,
+	    parseCountry,
+	    parseDate,
+	    parseSex,
+	    parseDocumentNumber,
+	    parseDocumentType
+	} = __webpack_require__(4);
+
+	module.exports = function parseTD3(lines) {
+	    var result = {isValid: true};
+	    var logs = [];
 	    logs.push('Parsing TD3 format');
 	    var first = lines[0];
 	    if (first.length !== 44) {
@@ -339,10 +408,9 @@ return /******/ (function(modules) { // webpackBootstrap
 	    if (result.isValid) result.isValid = check(second.substring(0, 10) + second.substring(13, 20) + second.substring(21, 43), second.substr(43, 1));
 
 	    logs.push('TD3 parse completed');
-	    return {
-	        logs,
-	        value: result
-	    };
+
+	    result.logs = logs;
+	    return result;
 	};
 
 
